0a45a51f3b4dc64d058ca35a3e4d555c2cd06c6d
Jullmenk <julinolady@gmail.com>
2025-05-14 22:06:47 +0100

    fix: adjust layout and styling in ReasonsToContact component for better responsiveness

--- src/components/templates/Home/reasons-to-contact.tsx
+++ src/components/templates/Home/reasons-to-contact.tsx
@@ -7,24 +7,26 @@ export default function ReasonsToContact() {
   return (
     <div className="w-full flex justify-center items-center bg-bg-2 py-16">
       <div className="w-full px-6 sm:px-0 sm:w-def flex flex-col gap-7">
-        <div className="relative w-full h-[400px]">
-          <Image
-            fill
-            src="/img/man-in-screen-talking-with-stranger.png"
-            alt="Homem numa videochamada"
-          />
+        <div className="flex justify-center items-center">
+          <div className="relative w-[80%] sm:w-full h-[250px] sm:h-[400px]">
+            <Image
+              fill
+              src="/img/man-in-screen-talking-with-stranger.png"
+              alt="Homem numa videochamada"
+            />
+          </div>
         </div>
 
-        <h2 className={`${crimsonText.className} text-2xl font-semibold mt-6 mb-2 text-gray-800`}>
+        <h2
+          className={`${crimsonText.className} text-2xl font-semibold mt-6 mb-2 text-gray-800`}
+        >
           {t("title")}
         </h2>
 
-        <p>
-          {t("text-1")}
-        </p>
+        <p>{t("text-1")}</p>
 
         <ul className="list-disc pl-6 space-y-3">
-          {Array.from({ length: 6}, (_, i) => (
+          {Array.from({ length: 6 }, (_, i) => (
             <li key={i}>{t(`li.li-${i + 1}`)}</li>
           ))}
         </ul>
